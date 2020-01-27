a8c990e390796bad7ff497020b0d6e7672df90b9
Amy Lam <amy.r.lam@gmail.com>
2020-01-27 11:50:13 -0800

    Update constants.js

--- app/constants.js
+++ app/constants.js
@@ -1,5 +1,5 @@
 const CONSTANTS = {
-  REPOS: ['emberjs', 'ember-cli', 'glimmerjs', 'ember-fastboot', 'ember-learn', 'ember-codemods'],
+  REPOS: ['emberjs', 'ember-cli', 'glimmerjs', 'ember-fastboot', 'ember-learn', 'ember-codemods', 'adopted-ember-addons'],
   // REPOS: ['ember-cli'], // limit no. of repos for testing (to not exhaust API RATE LIMIT so quickly)
 };
 
