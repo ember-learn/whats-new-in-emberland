0e9489f0e3fbe436bc6b5d0deb915f2bc9dbe2b0
ijlee2 <ijlee2@users.noreply.github.com>
2020-07-31 10:12:04 -0500

    Created <PullRequests> component

---
+++ tests/integration/components/pull-requests-test.js
@@ -0,0 +1,45 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { findAll, render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | pull-requests', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    this.pullRequests = [
+      {
+        title: 'Upgraded app to Ember Octane',
+        htmlUrl: 'https://github.com/ember-learn/whats-new-in-emberland/pull/37',
+        userLogin: 'zoey'
+      },
+      {
+        title: 'Documented how to locally run the app',
+        htmlUrl: 'https://github.com/ember-learn/whats-new-in-emberland/pull/39',
+        userLogin: 'tomster'
+      }
+    ];
+
+    await render(hbs`
+      {{pull-requests
+        pullRequests=this.pullRequests
+      }}
+    `);
+
+    const pullRequests = findAll('[data-test-pull-request]');
+
+    assert.strictEqual(pullRequests.length, 2, 'We see 2 pull requests.');
+
+    assert.dom('[data-test-field="Title"]', pullRequests[0])
+      .hasText(
+        'Upgraded app to Ember Octane',
+        'We see the correct title for the 1st pull request.'
+      );
+
+    assert.dom('[data-test-field="Title"]', pullRequests[1])
+      .hasText(
+        'Documented how to locally run the app',
+        'We see the correct title for the 2nd pull request.'
+      );
+  });
+});
---
+++ tests/integration/components/pull-requests/card-test.js
@@ -0,0 +1,48 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | pull-requests/card', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    this.PR = {
+      title: 'Upgraded app to Ember Octane',
+      htmlUrl: 'https://github.com/ember-learn/whats-new-in-emberland/pull/37',
+      userLogin: 'zoey'
+    };
+
+    await render(hbs`
+      {{pull-requests/card
+        PR=this.PR
+      }}
+    `);
+
+    assert.dom('[data-test-field="Title"]')
+      .hasText(
+        'Upgraded app to Ember Octane',
+        'We see the correct title.'
+      );
+
+    assert.dom('[data-test-field="Title"]')
+      .hasAttribute(
+        'href',
+        'https://github.com/ember-learn/whats-new-in-emberland/pull/37',
+        'We see the correct link to the PR.'
+      );
+
+    assert.dom('[data-test-field="Author"]')
+      .hasText(
+        '@zoey',
+        'We see the correct author.'
+      );
+
+    assert.dom('[data-test-field="Author"]')
+      .hasAttribute(
+        'href',
+        'https://github.com/zoey',
+        'We see the correct link to the author\'s GitHub page.'
+      );
+  });
+});
