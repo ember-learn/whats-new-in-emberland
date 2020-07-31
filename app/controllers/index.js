62dbe9b77e97d383ca2679478262478121b4ee44
ijlee2 <ijlee2@users.noreply.github.com>
2020-07-31 14:38:39 -0500

    Refactored getContributors by extracting functions

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,46 +7,54 @@ export default Controller.extend({
   actions: {
     async getContributors() {
       const { mergedPulls } = this.model;
-
       const fetchRequests = mergedPulls.map(pull => pull.get('user'));
-      const users = await all(fetchRequests);
 
-      // Remove users that are bots or appeared more than once
-      const uniqueUsers = users
-        .reduce((accumulator, user) => {
-          const { htmlUrl, login: username, type } = user;
+      let users = await all(fetchRequests);
+      users = this.identifyUsers(users);
+      users = this.sortUsers(users);
 
-          const isNotBot = (type === 'User');
-          const isNotDuplicate = !accumulator.has(username);
-
-          if (isNotBot && isNotDuplicate) {
-            accumulator.set(username, {
-              handle: `@${username}`,
-              profileLink: htmlUrl
-            });
-          }
-
-          return accumulator;
-
-        }, new Map());
-
-      // Sort users by handle
-      const sortedUsers = Array.from(uniqueUsers.values())
-        .sort((a, b) => {
-          if (a.handle > b.handle) return 1;
-          if (a.handle < b.handle) return -1;
-          return 0;
-        });
-
-      // Create the contributor list
-      const contributorsList = sortedUsers
-        .map(user => {
-          const { handle, profileLink } = user;
-          return `<a href="${profileLink}" target="_blank">${handle}</a>`;
-        })
-        .join(', ');
-
-      this.set('contributorsList', contributorsList);
+      this.updateContributorsList(users);
     }
+  },
+
+  identifyUsers(users) {
+    // Remove users that are bots or appeared more than once
+    const uniqueUsers = users.reduce((accumulator, user) => {
+      const { htmlUrl, login: username, type } = user;
+
+      const isNotBot = (type === 'User');
+      const isNotDuplicate = !accumulator.has(username);
+
+      if (isNotBot && isNotDuplicate) {
+        accumulator.set(username, {
+          handle: `@${username}`,
+          profileLink: htmlUrl
+        });
+      }
+
+      return accumulator;
+
+    }, new Map());
+
+    return Array.from(uniqueUsers.values());
+  },
+
+  sortUsers(users) {
+    return users.sort((a, b) => {
+      if (a.handle > b.handle) return 1;
+      if (a.handle < b.handle) return -1;
+      return 0;
+    });
+  },
+
+  updateContributorsList(users) {
+    const contributorsList = users
+      .map(user => {
+        const { handle, profileLink } = user;
+        return `<a href="${profileLink}" rel="noopener noreferrer" target="_blank">${handle}</a>`;
+      })
+      .join(', ');
+
+    this.set('contributorsList', contributorsList);
   }
 });
