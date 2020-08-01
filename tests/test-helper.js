778da116da57b916236720445255766df9a830d1
abhilashlr <abhilashlr.789@gmail.com>
2020-08-01 15:55:58 +0530

    chore(migration): upgrades to 3.20

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'whats-new-in-emberland/app';
+import config from 'whats-new-in-emberland/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
