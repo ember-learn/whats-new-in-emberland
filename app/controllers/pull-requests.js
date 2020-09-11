247d97811e667c4df180dc553c3d810297918166
ijlee2 <ijlee2@users.noreply.github.com>
2020-09-11 11:27:02 -0400

    Passed GitHub token to allow calling fetchAdditionalUserDetails for many users

--- app/controllers/pull-requests.js
+++ app/controllers/pull-requests.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { all } from 'rsvp';
 
@@ -9,6 +10,8 @@ export default class PullRequestsController extends Controller {
   @tracked mergedPRs = [];
   @tracked newPRs = [];
   @tracked contributorsList = '';
+
+  @service githubSession;
 
   @action
   async getContributors() {
@@ -56,7 +59,14 @@ export default class PullRequestsController extends Controller {
 
   async fetchAdditionalUserDetails(users) {
     let userPromises = users.map((user) => {
-      return fetch(user.url).then((response) => response.json());
+      return fetch(user.url, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': `token ${this.githubSession.githubAccessToken}`,
+        }
+      })
+        .then((response) => response.json());
     });
 
     let userDataResponses = await all(userPromises);
