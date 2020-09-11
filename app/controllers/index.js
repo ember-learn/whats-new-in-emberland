f18fdbda6ea8729d88c2088d51c72409103451c8
ijlee2 <ijlee2@users.noreply.github.com>
2020-09-11 10:57:49 -0400

    Created index route to display <SearchForm> component

---
+++ app/controllers/index.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
+export default class IndexController extends Controller {
+  @action fetchPullRequests({ mergedSince }) {
+    this.transitionToRoute('pull-requests', {
+      queryParams: {
+        mergedSince,
+      }
+    });
+  }
+}
