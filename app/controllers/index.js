90a9de11e1dad87dc1276aa10dda93aec4c63a12
Isaac Lee <ijlee2@users.noreply.github.com>
2021-08-18 07:20:54 +0200

    Addressed linting errors and a deprecation warning

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class IndexController extends Controller {
+  @service router;
+
   @action fetchPullRequests({ mergedSince }) {
-    this.transitionToRoute('pull-requests', {
+    this.router.transitionTo('pull-requests', {
       queryParams: {
         mergedSince,
       },
